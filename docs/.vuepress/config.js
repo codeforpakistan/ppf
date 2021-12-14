953f2e9638bec44e106b218492c661800fc9a51d
Ali Raza <dejavu87@gmail.com>
2021-12-14 15:52:14 +0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  title: 'Hello, World.',
-  description: '📦 🎨 A api-friendly theme for VuePress.',
+  title: 'Framework for Digitally enabled citizen-led participatory processes',
+  description: 'Prepared by Codeando Mexico and Code for Pakistan for the Code for All Exchange Programme',
   theme: 'api',
+  base :'/ppf/'
 }
