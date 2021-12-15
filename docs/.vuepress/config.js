b0555fb343d5b71414a081c9d76333fe2dec2b4c
Ebtihaj <ebtihaj_316@yahoo.com>
2021-12-15 16:30:15 +0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,6 @@ module.exports = {
   theme: 'api',
   base: '/ppf/',
   plugins: [
-    ['@vuepress/plugin-google-analytics', { ga: 'G-ZPPP4EYWV7' }],
+    ['@vuepress/google-analytics', { ga: 'G-ZPPP4EYWV7' }],
   ],
 }
